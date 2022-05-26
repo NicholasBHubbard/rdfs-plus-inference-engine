2c6800f5c8270e9cf03993d399fbf1325a99aeaf
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-05-26 17:16:46 -0400

    Ready for testing.

--- main.pl
+++ main.pl
@@ -1,13 +1,23 @@
+#!/usr/bin/env -S swipl -q -f none -g main
+
 %   Author: Nicholas Hubbard
-%   Email:  nhub73@keemail.me
+%   Email:  nicholashubbard@posteo.net
 %   WWW:    https://github.com/NicholasBHubbard/rdf_inference_engine
 
 :- use_module(ttl_read_write).
 :- use_module(inference).
 :- use_module(contradiction).
 
-main(File) :-
+main :-
+    current_prolog_flag(argv, ARGV),
+    main(ARGV),
+    halt(0).
+
+main([]).
+main(Files) :-
+    retractall(rdf(_,_,_)),
     parse_and_assert_ttlFile(File),
-%    make_all_inferences, LAST THING THAT NEEDS TO BE DONE
+    make_all_inferences,
     check_for_contradictions,
-    write_new_ttlFile.
+    write_new_ttlFile,
+    main(Files).
